b05352d5bcee001ca2a04b246478599c9d79960b
Etienne Soucy <soucy.etienne@courrier.uqam.ca>
2022-01-22 15:47:32 -0500

    pre-reimplementation commit

---
+++ domydrafter/data/nation_pools.py
@@ -0,0 +1,9 @@
+from .config import data_dir
+from .utils import read_csv
+
+from .nation import Nation
+from .site import Site
+from .unit import Unit
+
+from typing import Any, Dict, List, Tuple
+from functools import reduce
